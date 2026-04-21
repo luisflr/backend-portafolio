a3b77937400f7c6bca50ecab28a77adef97fa701
luisflr <lfloresr@ulasalle.edu.pe>
2026-04-21 11:28:30 -0500

    added trust proxy setting

--- src/index.js
+++ src/index.js
@@ -1,63 +1,62 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv'
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 // middleware
-import { apiLimiter } from './middleware.js';
+import { apiLimiter } from "./middleware.js";
 
 // importamos las rutas
-import project_routes from './routes/projects.js';
-import work_routes from './routes/works.js'
+import project_routes from "./routes/projects.js";
+import work_routes from "./routes/works.js";
 
 // instalamos dotenv y lo utilizamos para acceder a variables de entorno
 dotenv.config();
 
 const WHITE_LIST = [
-  'http://localhost:8800',
-  'http://localhost:8080',
-  'http://localhost:3000',
-  'http://localhost:1234',
-  'https://luisgfr.com',
-  'https://www.luisgfr.com/',
-  'https://www.luisgfr.com',
-]
-
-
+  "http://localhost:8800",
+  "http://localhost:8080",
+  "http://localhost:3000",
+  "http://localhost:1234",
+  "https://luisgfr.com",
+  "https://www.luisgfr.com/",
+  "https://www.luisgfr.com",
+];
 
 // el metodo express() retorna el método de la aplicacion
 const app = express();
 
+app.set("trust proxy", 1);
+
 const PORT = process.env.PORT || 8080;
 
 // middlewares
-app.use(cors({
-  origin: (origin, callback) => {
-    if (WHITE_LIST.includes(origin) || !origin) {
-      return callback(null, true)
-    }
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (WHITE_LIST.includes(origin) || !origin) {
+        return callback(null, true);
+      }
 
-    return callback(new Error('Not allowed by CORS'))
-  }
-}));
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
 
-app.use(express.json(), apiLimiter)
+app.use(express.json(), apiLimiter);
 
-app.use('/api/v1', project_routes);
-app.use('/api/v1', work_routes);
+app.use("/api/v1", project_routes);
+app.use("/api/v1", work_routes);
 
 //routes
-app.get('/', (req, res) =>{
-  res.send('Proyecto backend portafolio node/express/mongoose')
+app.get("/", (req, res) => {
+  res.send("Proyecto backend portafolio node/express/mongoose");
 });
-
-
 
 // mongodb connection
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log('connected to db atlas'))
+  .then(() => console.log("connected to db atlas"))
   .catch((err) => console.error(err));
 
-app.listen(PORT, () => console.log('server is listening on port', PORT));
-
+app.listen(PORT, () => console.log("server is listening on port", PORT));
